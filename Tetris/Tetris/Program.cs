59169a8a77a7fd9f565b1d87ddf721598a2bb3d5
Dima <dbjenov@gmail.com>
2021-01-31 13:10:24 +0200

    Lesson2

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,8 +6,26 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            !!!
+
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            Console.ReadLine();
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Draw(x1, y1,c1);
+
+
+
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(c);
         }
     }
 }
